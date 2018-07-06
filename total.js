89d588f5783f0dfe48f61fa04ed6e60028fd5982
richard <richardchankiyin@gmail.com>
2018-07-06 16:19:14 +0800

    add mkdir out_total

--- total.js
+++ total.js
@@ -2,6 +2,7 @@ var request=require("request");
 var fs=require("fs");
 
 var url=fs.readFileSync("_url") + "&startAt=0&maxResults=1";
+fs.mkdirSync("out_total");
 var out="out_total/out.log";
 
 function getTotal(url,out) {
